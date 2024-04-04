c645ad672c6cbfa7d8ea76ead271202df57923dd
Steven Hanson <stevenahanson@live.com>
2024-04-04 15:40:19 +0100

    display photos in a grid

---
+++ photos-api/next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+    domains: ['via.placeholder.com'], // Add the domain(s) here
+  },
+};
--- photos-api/src/app/page.tsx
+++ photos-api/src/app/page.tsx
@@ -1,5 +1,37 @@
-export default function Home() {
-  return <p>Hello World</p>;
+"use client";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+
+interface Photos {
+  id: string;
+  url: string;
+  title: string;
 }
 
-// endpoint: https://jsonplaceholder.typicode.com/photos
+export default function Home() {
+  const [photos, setPhotos] = useState<Photos[]>([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data: Photos[]) => {
+        setPhotos(data);
+      });
+  }, []);
+
+  return (
+    <div className="grid grid-cols-12">
+      {photos.map((photos) => (
+        <Image
+          key={photos.id}
+          src={photos.url}
+          alt={photos.title}
+          height={100}
+          width={100}
+        />
+      ))}
+    </div>
+  );
+}
